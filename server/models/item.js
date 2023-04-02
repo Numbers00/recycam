0600116906b9f1fb9ae7f539cadf9053179cb276
Numbers00 <kmaquino@up.edu.ph>
2023-04-02 21:27:47 +0800

    Extract the Option model from the Item model, create the User model

--- server/models/item.js
+++ server/models/item.js
@@ -1,35 +1,13 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-const optionSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  }
-});
-
 const itemSchema = new mongoose.Schema({
   name: {
     type: String,
     required: 'the item\'s name is required'
   },
-  best_option: {
-    name: {
-      type: String,
-      required: 'the best option\'s name is required'
-    },
-    description: {
-      type: String,
-      required: 'the best option\'s description is required'
-    }
-  },
-  other_options: {
-    type: [optionSchema],
-    required: false
+  options: {
+    type: [optionSchema]
   }
 });
 

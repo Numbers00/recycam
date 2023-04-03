70b64b2f3de3cdf65660ddf59ff074533e4c623b
Numbers00 <kmaquino@up.edu.ph>
2023-04-03 16:55:23 +0800

    Set up font and img assets

--- RecyCam/App.js
+++ RecyCam/App.js
@@ -1,20 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import * as Font from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
+import {
+  setCustomText
+} from 'react-native-global-props';
+
+import { useCallback, useEffect, useState } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+SplashScreen.preventAutoHideAsync();
+
+const App = () => {
+
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync({
+          'Roboto': require('./assets/fonts/Roboto/Roboto-Regular.ttf'),
+        });
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+      } catch (err) {
+        console.warn(err);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const setDefaultFont = () => {
+    const customTextProps = {
+      style: {
+        fontFamily: 'Roboto'
+      }
+    };
+    setCustomText(customTextProps);
+  };
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+
+      setDefaultFont();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) return null;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView
+      style={styles.mainContainer}
+      onLayout={onLayoutRootView}
+    >
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    position: 'relative',
+    display: 'flex',
+    flexDirection: 'column'
   },
 });
+
+export default App;
